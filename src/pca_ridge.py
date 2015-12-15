a1ad68b8815638f93c8ec93b2d2dc3dea6083daa
Abhishek Bhatia <abhatia1@andrew.cmu.edu>
2015-12-14 23:30:29 -0500

    Updated to version that got current highest score.

--- src/pca_ridge.py
+++ src/pca_ridge.py
@@ -14,7 +14,7 @@ from sklearn import preprocessing
 from sklearn.feature_selection import SelectKBest
 from sklearn.pipeline import Pipeline, FeatureUnion
 import matplotlib.cm as cm
-from sklearn.kernel_ridge import KernelRidge
+#from sklearn.kernel_ridge import KernelRidge
 from sklearn.linear_model import Ridge, LogisticRegression, LinearRegression, Lasso
 
 ''' Notes
@@ -33,7 +33,7 @@ from sklearn.linear_model import Ridge, LogisticRegression, LinearRegression, La
     
 end Notes'''
 
-file_loc = '/home/apark/Homework/ML_Project2/data/'
+file_loc = '/home/abhishekb/ML_Project2/data/'
 
 ## Import data
 import_test = sio.loadmat(file_loc + 'Test.mat')
@@ -42,16 +42,23 @@ import_providedidx = sio.loadmat(file_loc + 'provideIdx.mat')
 import_missidx = sio.loadmat(file_loc + 'missIdx.mat')
 import_providedata = sio.loadmat(file_loc + 'provideData_1000.mat')
 import_events = sio.loadmat(file_loc + 'events_1000.mat')
+import_test_labels = sio.loadmat(file_loc + 'Ytest.mat')
 
 provideIdx = import_providedidx['provideIdx'] - 1 # since Matlab indexing starts at 1
 provideIdx = provideIdx.ravel()
 missIdx = import_missidx['missIdx'] - 1 # since Matlab indexing starts at 1
 missIdx = missIdx.ravel()
 events = import_events['events']
+X_test_raw_1 = import_test['Xtest']
 X_test_raw = import_providedata['provideData'] # size = (1000, 3172)
 XY_train_raw = import_train['Xtrain']
-X_train_raw = XY_train_raw[:,provideIdx] # size = (500, 3172)
-Y_train_raw = XY_train_raw[:,missIdx] # size = (500, 2731)
+X_train_raw_2 = XY_train_raw[:,provideIdx] # size = (500, 3172)
+X_train_raw_1 = X_test_raw_1[:,provideIdx]  
+Y_train_raw_2 = XY_train_raw[:,missIdx] # size = (500, 2731)
+Y_train_raw_1 = X_test_raw_1[:,missIdx]
+X_train_raw = np.vstack((X_train_raw_1, X_train_raw_2))
+Y_train_raw = np.vstack((Y_train_raw_1, Y_train_raw_2))
+train_labels = np.vstack((import_test_labels['Ytest'],import_train['Ytrain'])) #labels of the original train data
 
 ## Standardization
 scaler = preprocessing.StandardScaler().fit(X_train_raw)
@@ -59,11 +66,21 @@ X_train_scaled = scaler.transform(X_train_raw)
 X_test_scaled = scaler.transform(X_test_raw)
 
 ## PCA and Feature Selection
-pca = PCA(n_components=200)
+
+'''pca = PCA(n_components=100)  
 pca.fit(X_train_scaled)
-print(pca.explained_variance_ratio_) 
+#print(pca.explained_variance_ratio_) 
 X_train_reduced = pca.transform(X_train_scaled)
 X_test_reduced = pca.transform(X_test_scaled)
+'''
+
+pca = PCA(n_components=800)
+selection = SelectKBest(k=850)
+combined_features = FeatureUnion([("pca", pca), ("univ_select", selection)])
+combined_features.fit(X_train_scaled, train_labels.ravel())
+#print(pca.explained_variance_ratio_) 
+X_train_reduced = combined_features.transform(X_train_scaled)
+X_test_reduced = combined_features.transform(X_test_scaled)
 
 ## Create K folds
 k_fold = KFold(Y_train_raw.shape[0], n_folds=10)
@@ -77,7 +94,7 @@ for train, test in k_fold:
     ## Train Classifiers on fold
     rdg_clf = Ridge(alpha=.5)
     rdg_clf.fit(X1,Y1)
-    lso_clf = Lasso(alpha=.1)
+    lso_clf = Lasso(alpha=.05)
     lso_clf.fit(X1,Y1)
         
     ## Score Classifiers on fold
@@ -89,7 +106,8 @@ for train, test in k_fold:
 
 
 ## Train final Classifiers
-clf = Ridge(alpha=.5)
+#clf = Ridge(alpha=.5)
+clf = Lasso(alpha=.05)
 clf.fit(X_train_reduced, Y_train_raw)
 Y_predicted = clf.predict(X_test_reduced)
 
