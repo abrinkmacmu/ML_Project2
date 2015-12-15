308aeb5d63181a6e8940dc84b876c3ff9b01b3d3
abrinkmacmu <abrinkma@andrew.cmu.edu>
2015-12-15 17:17:29 -0500

    tried everything with clustering, no luck

--- src/clustering_v2.py
+++ src/clustering_v2.py
@@ -21,22 +21,10 @@ from sklearn.linear_model import Ridge, LogisticRegression, LinearRegression, La
     1)
         
     Results
-    test_median
-    Radius = 1, STD threshold = .2: .4% replaced, no significant change to accuracy
-    Radius = 1, STD threshold = .25: 2.2% replaced, slight decrease in accuracy
-    
-    test_mean
-    Radius = 1, STD threshold = .25, 2.2% replaced, slight decrease in accuracy
-    
-    compromise (Ridge/Mean)
-    Radius = 1, STD threshold = .25, 2.2% replace, no change
-    Radius = 1, STD threshold = .30, 7.32% replaced, slight improvement rmse = .53525
-    Radius = 1, STD threshold = .35, 16.583% replaced, improvement      rmse = .534637
     
     compromise (Ridge/Median)
-    Radius = 1, STD threshold = .30, 7.32% replaced, slight improvement rmse = .535292
-    Radius = 1, STD threshold = .35, 16.583% replaced, improvement      rmse = .534717
-    Radius = 1, STD threshold = .35, 28.81% replaced, improvement       rmse = .534212
+    Radius = 1, STD threshold = .4,replaced, improvement rmse =.48912723628648319 
+    Radius = 1, STD threshold = .2 replaced, improvement rmse =.48626845804644109
     
 end Notes'''
 
@@ -49,16 +37,23 @@ import_providedidx = sio.loadmat(file_loc + 'provideIdx.mat')
 import_missidx = sio.loadmat(file_loc + 'missIdx.mat')
 import_providedata = sio.loadmat(file_loc + 'provideData_1000.mat')
 import_events = sio.loadmat(file_loc + 'events_1000.mat')
+import_test_labels = sio.loadmat(file_loc + 'Ytest.mat')
 
 provideIdx = import_providedidx['provideIdx'] - 1 # since Matlab indexing starts at 1
 provideIdx = provideIdx.ravel()
 missIdx = import_missidx['missIdx'] - 1 # since Matlab indexing starts at 1
 missIdx = missIdx.ravel()
 events = import_events['events']
+X_test_raw_1 = import_test['Xtest']
 X_test_raw = import_providedata['provideData'] # size = (1000, 3172)
 XY_train_raw = import_train['Xtrain']
-X_train_raw = XY_train_raw[:,provideIdx] # size = (500, 3172)
-Y_train_raw = XY_train_raw[:,missIdx] # size = (500, 2731)
+X_train_raw_2 = XY_train_raw[:,provideIdx] # size = (500, 3172)
+X_train_raw_1 = X_test_raw_1[:,provideIdx]  
+Y_train_raw_2 = XY_train_raw[:,missIdx] # size = (500, 2731)
+Y_train_raw_1 = X_test_raw_1[:,missIdx]
+X_train_raw = np.vstack((X_train_raw_1, X_train_raw_2))
+Y_train_raw = np.vstack((Y_train_raw_1, Y_train_raw_2))
+train_labels = np.vstack((import_test_labels['Ytest'],import_train['Ytrain'])) #labels of the original train data
 x = import_train['x']
 y = import_train['y']
 z = import_train['z']
@@ -69,24 +64,35 @@ X_train_scaled = scaler.transform(X_train_raw)
 X_test_scaled = scaler.transform(X_test_raw)
 
 ## PCA and Feature Selection
-pca = PCA(n_components=100)
-selection = SelectKBest(k=50)
-combined_features = FeatureUnion([("pca", pca), ("univ_select", selection)])
-flaty = np.zeros([501,1])
-combined_features.fit(X_train_scaled,flaty)
-print(pca.explained_variance_ratio_) 
+
+'''pca = PCA(n_components=100)  
+pca.fit(X_train_scaled)
+#print(pca.explained_variance_ratio_) 
 X_train_reduced = pca.transform(X_train_scaled)
 X_test_reduced = pca.transform(X_test_scaled)
+'''
+print 'Data imported and scaled'
+pca = PCA(n_components=800)
+selection = SelectKBest(k=850)
+combined_features = FeatureUnion([("pca", pca), ("univ_select", selection)])
+combined_features.fit(X_train_scaled, train_labels.ravel())
+#print(pca.explained_variance_ratio_) 
+X_train_reduced = combined_features.transform(X_train_scaled)
+X_test_reduced = combined_features.transform(X_test_scaled)
 
-## Train final Classifier
-clf = Ridge(alpha=.5)
+print 'Feauture selection complete'
+
+## Train final Classifiers
+#clf = Ridge(alpha=.5)
+clf = Lasso(alpha=.03)
 clf.fit(X_train_reduced, Y_train_raw)
+print 'fitted data to classifier'
 Y_predicted = clf.predict(X_test_reduced)
-
+print 'classification complete'
 ## clustering algorithm
-
+print 'begin clustering'
 RADIUS = 1.0
-STD_THRESHOLD = .4
+STD_THRESHOLD = .15
 n_train_samples = X_train_raw.shape[0]
 n_test_samples = X_test_raw.shape[0]
 n_features = provideIdx.shape[0]
